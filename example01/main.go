fc839a684f2c96055f9333e9b78cdf3e50fb4fab
duonghuu <huuduong9@gmail.com>
2025-11-19 07:29:30 +0700

    ngay01

--- example01/main.go
+++ example01/main.go
@@ -1,4 +1,27 @@
 package main
 
+import "fmt"
+
+func sum(a, b int) int {
+	return a + b
+}
+
+func sayHello() {
+
+}
+
 func main() {
+	var name string = "eric"
+	var age int = 10
+	var gender bool = false
+	const PI = 3.14
+	genderText := ""
+	if gender == false {
+		genderText = "Nam"
+	} else {
+		genderText = "Nữ"
+	}
+
+	fmt.Printf("%s %d %s\n", name, age, genderText)
+	fmt.Printf("answer: %.2f", PI)
 }
